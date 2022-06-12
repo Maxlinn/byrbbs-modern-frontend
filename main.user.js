940e7a233a94a1ab0c59754ecbbafb852cf73fda
Maxlinn <maxlinn@foxmail.com>
2022-06-13 02:56:22 +0800

    存在恶性bug，请勿更新

--- main.user.js
+++ main.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         北邮人论坛 - 变好看！
 // @namespace    http://github.com/Maxlinn/
-// @version      2.0.0
+// @version      1.2.0
 // @description  一些仅在前端的北邮人论坛美化
 // @author       Maxlinn
 // @match        https://bbs.byr.cn/
